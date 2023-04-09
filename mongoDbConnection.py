a628e87da7547d135cdb7f383628ab27bddd0117
Gurtok <harocolorado@gmail.com>
2023-04-09 15:39:42 -0600

    Mongo Helper Functions

--- mongoDbConnection.py
+++ mongoDbConnection.py
@@ -1,11 +1,41 @@
-import pymongo
+import pymongo, sys
 
 
 devMongoDbAddress = ("mongodb://localhost:42000/")
 collectionName = "testCollection"
 
-def dbConnection():
-    mongoClient = pymongo.MongoClient(devMongoDbAddress)
-    db = mongoClient['basicDB']
+monogoClient = ""
+db = ""
+
+
+def __init__():
+    try:
+        mongoClient = pymongo.MongoClient(devMongoDbAddress)
+        db = mongoClient['basicDB']
+    except Exception as e:
+        print("Failed to connect to MongoDB @", devMongoDbAddress)
+        print(e)
+        sys.exit()
+
+
+# Called by monboDbInsert & mongoDbRead classes
+# All instances should call this method to avoid multiple DB connections
+# Return: Object to the database for read/writes
+def get_DB():
+    if db != None:
+        print("Connected to Mongo: ", devMongoDbAddress)
+        return db
+    else:
+        print("DB connection failed")
+
+
+# Called by mongoDbRead
+# Returns object of type COLLECTION
+def get_DB_Collection():
     collection = db[collectionName]
-    return collection
+    if collection != None:
+        return collection
+    else:
+        print("DB Collection is NULL")
+
+
